bee5a5ba35115901ee888a9b4ffe8f5cb4905788
Gaston Martinez <gastonmartinez8596@gmail.com>
2026-02-27 20:00:51 -0300

    Git ignore más ultimos cambios

--- client/dist-storehilos/assets/apiConfig-f250f70e.js
+++
@@ -1,2 +0,0 @@
-import{l as t}from"./index-97ba732a.js";const i=async()=>{try{return(await t()).apiUrl||window.location.origin}catch(o){return console.error("Error loading config for API URL, using fallback:",o),"http://localhost:5175"}},c=async o=>{const r=await i(),n=o.startsWith("/")?o:`/${o}`;return`${r}${n}`};export{c as buildApiUrl,i as getApiBaseUrl};
-//# sourceMappingURL=apiConfig-f250f70e.js.map
